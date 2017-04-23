1fff2de84a77a4cf2175eea323e14bad2683e231
Lauri Mäkinen <lauri@mits.fi>
2017-04-23 11:38:23 +0300

    FK constraints

--- schema.sql
+++ schema.sql
@@ -1,17 +1,7 @@
-CREATE TABLE people (`id` int, `name` varchar(100), `age` int, `city_id` int);
-
-INSERT INTO people
-  (`id`, `name`, `age`, `city_id`)
-VALUES
-  (1, "Lauri", NULL, 1),
-  (2, "Mariel", 22, 1),
-  (3, "Keimo", 52, NULL),
-  (4, "Keimo", 12, 8),
-  (5, "Aada", 6, 3),
-  (6, "Tarja", 38, 2)
-;
-
-CREATE TABLE cities (`id` int, `name` varchar(100));
+CREATE TABLE cities (
+  `id` INTEGER PRIMARY KEY,
+  `name` VARCHAR(100)
+);
 
 INSERT INTO cities
   (`id`, `name`)
@@ -19,4 +9,23 @@ VALUES
   (1, "Espoo"),
   (2, "Helsinki"),
   (3, "Tampere")
+;
+
+CREATE TABLE people (
+  `id` INTEGER PRIMARY KEY,
+  `name` varchar(100),
+  `age` INTEGER,
+  `city_id` INTEGER,
+  CONSTRAINT FOREIGN KEY (city_id) REFERENCES cities (id)
+);
+
+INSERT INTO people
+  (`id`, `name`, `age`, `city_id`)
+VALUES
+  (1, "Lauri", NULL, 1),
+  (2, "Mariel", 22, 1),
+  (3, "Keimo", 52, 2),
+  (4, "Keimo", 12, 3),
+  (5, "Aada", 6, 3),
+  (6, "Tarja", 38, 2)
 ;
