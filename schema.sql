c5777e37815d66029f8e55ec83304ae9a5047c86
Lauri Mäkinen <lauri@mits.fi>
2017-04-23 08:48:43 +0300

    Initial commit

---
+++ schema.sql
@@ -0,0 +1,12 @@
+CREATE TABLE people ("id" int, "name" varchar(100), "age" int);
+
+INSERT INTO people
+  ("id", "name", "age")
+VALUES
+  (1, "Lauri", NULL),
+  (2, "Mariel", 22),
+  (3, "Keimo", 52),
+  (4, "Keimo", 12),
+  (5, "Aada", 6),
+  (6, "Tarja", 38)
+;
