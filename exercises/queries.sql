96a7d7e23091f2fa68edbc3dcad530e46fafc668
Lauri Mäkinen <lauri@mits.fi>
2017-04-23 09:05:16 +0300

    COUNT

--- exercises/queries.sql
+++ exercises/queries.sql
@@ -7,20 +7,24 @@ SELECT p.name, p.age
   FROM people p
     WHERE p.age IS NOT null;
 
+
 -- BETWEEN
 SELECT p.name, p.age
   FROM people p
     WHERE p.age BETWEEN 10 AND 40;
+
 
 -- LIKE
 SELECT p.name, p.age
   FROM people p
     WHERE p.name LIKE "%ar%";
 
+
 -- IN
 SELECT p.name, p.age
   FROM people p
     WHERE p.name IN ("Lauri", "Mariel");
+
 
 -- ORDER BY
 SELECT p.name, p.age
@@ -31,3 +35,12 @@ SELECT p.name, p.age
 SELECT p.name, p.age
   FROM people p
     ORDER BY p.name;
+
+
+-- COUNT (* counts all rows, including null values)
+SELECT COUNT(*) as NumOfPeople
+  FROM people p;
+
+-- Counting by column, excludes null values
+SELECT COUNT(p.age) as NumOfPeopleWithAge
+  FROM people p;
